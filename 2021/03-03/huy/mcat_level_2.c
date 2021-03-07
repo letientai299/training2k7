b282bf34504a0840bd47b35ed579b6ab3eb16210
Tai <letientai299@gmail.com>
2021-03-07 15:45:49 +0800

    Add Huy's solution for level 2 of mcat
    Sample output of current solution when run the program against its
    source code:
    
      +-----------------------------------------------------+
     1| #include <stdio.h>
     2| #include <stdlib.h>
     3| #include <string.h>
     4|
     5| void Row(int LineNumber, int max)
     6| {
     7|   if (LineNumber > 99) {
     8|     printf("%s", "   ");
    ...
    92|       }
    93|     }
    94|   }
    95|   fclose(file1);
    96|   printf("%c", '\n');
    97|   Row(LineNumber, max);
    98|   return 0;
    99| }
    
      +-----------------------------------------------------+
    
    Current code has some issues:
    
    - The bottom horizonal lines has one empty line before it.
    - The code is not well structured and don't have any comments.
    
    We will fix these issues in the next commits

---
+++ 2021/03-03/huy/mcat_level_2.c
@@ -0,0 +1,99 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+void Row(int LineNumber, int max)
+{
+  if (LineNumber > 99) {
+    printf("%s", "   ");
+    for (int i = 0; i <= max; ++i) {
+      if (i == 0 || i == max) {
+        printf("%c", '+');
+      } else {
+        printf("%c", '-');
+      }
+    }
+  } else {
+    if (LineNumber > 9) {
+      printf("%s", "  ");
+      for (int i = 0; i <= max; ++i) {
+        if (i == 0 || i == max) {
+          printf("%c", '+');
+        } else {
+          printf("%c", '-');
+        }
+      }
+    } else {
+      printf("%s", " ");
+      for (int i = 0; i <= max; ++i) {
+        if (i == 0 || i == max) {
+          printf("%c", '+');
+        } else {
+          printf("%c", '-');
+        }
+      }
+    }
+  }
+}
+
+int main(int argc, char* argv[])
+{
+  int i = 1, LineNumber = 0, max = 0, column;
+  if (argc == 1) {
+    printf("You need me");
+    return 0;
+  }
+  char const* filename = argv[1];
+  FILE* file = fopen(filename, "r");
+  char line[512];
+  while (fgets(line, sizeof(line), file)) {
+    if (max < strlen(line)) {
+      max = strlen(line);
+    }
+    LineNumber++;
+  }
+  fclose(file);
+  max += 3;
+  Row(LineNumber, max);
+  printf("%c", '\n');
+  FILE* file1 = fopen(filename, "r");
+  if (LineNumber < 1000 && LineNumber > 99) {
+    while (fgets(line, sizeof(line), file1)) {
+      if (i > 99) {
+        printf("%d| %s", i, line);
+        i++;
+      }
+      if (i < 100 && i > 9) {
+        printf(" %d| %s", i, line);
+        i++;
+      }
+      if (i < 10) {
+        printf("  %d| %s", i, line);
+        i++;
+      }
+    }
+  } else {
+    if (LineNumber < 100 && LineNumber > 9) {
+      while (fgets(line, sizeof(line), file1)) {
+        if (i >= 10) {
+          printf("%d| %s", i, line);
+          i++;
+        }
+        if (i <= 9) {
+          printf(" %d| %s", i, line);
+          i++;
+        }
+      }
+    } else {
+      if (LineNumber < 10) {
+        while (fgets(line, sizeof(line), file1)) {
+          printf("%d| %s", i, line);
+        }
+      }
+    }
+  }
+  fclose(file1);
+  printf("%c", '\n');
+  Row(LineNumber, max);
+  return 0;
+}
