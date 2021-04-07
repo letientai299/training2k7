8cace9888f9335b0ab3b00a71f7e03bd4473be73
Tai <letientai299@gmail.com>
2021-04-07 22:10:11 +0800

    Remove document part from the code to make it compilable

--- 2021/03-03/mcat.c
+++ 2021/03-03/mcat.c
@@ -28,7 +28,6 @@ int main(int argc, char* argv[])
   while (fgets(line, sizeof(line), file)) {
     printf("%d| %s", lineNum, line);
     lineNum++;
-    break;
   }
 
   // fclose: close the opening file
@@ -36,53 +35,3 @@ int main(int argc, char* argv[])
   fclose(file);
   return 0;
 }
-
-byte[8] byteArray;
-char[8] charArray;
-short[4] shorts; //  1 short = 2 bytes
-int[4] ints;     //  1 int = 4 bytes
-
-1 word = 4 bytes = 1 int = 1 float = 1 size_t = 1 unsigned int = ...
-
-8 * 5 + 3 = 43
-43 / 8 = 5
-43 % 8 = 3
-
-byteArray=8
-byteArray[i] -> doc data cua phan tu thu i ben trong array
-*byteArray = byteArray[0]
-byteArray[3]
-
-ints=36
-*ints = ints[0]
-ints[2]
-*(ints+sizeof(int))   = ints[1]
-*(ints+sizeof(int)*2) = ints[2]
-*(ints+sizeof(int)*3) = ints[3]
-
-*(36 + 4)
-*(40)
-
-Memory Address
-Pointer: con tro tro den 1 vung nho (memory area)
-Dereference
-Pointer arithmetic
-
-Row\Column:  0|     1|     2|     3|     4|     5|     6|     7|
- 0     | Byte | Byte | Byte | Byte | Byte | Byte | Byte | Byte |
- 1     | Byte | Byte | Byte | Byte | Byte | Byte | Byte | Byte |
- 2     | Byte | Byte | Byte | Byte | Byte | Byte | Byte | Byte |
- 3     | Byte | Byte | Byte | Byte | Byte | Byte | Byte | Byte |
- 4     | Byte | Byte | Byte | Byte |*ints*| Byte | Byte | Byte |
- 5     | Byte | Byte | Byte | Byte | Byte | Byte | Byte | Byte |
- 6     | Byte | Byte | Byte | Byte | Byte | Byte | Byte | Byte |
- 7     | Byte |*Byte*| Byte | Byte | Byte | Byte | Byte | Byte |
- 8     | Byte | Byte | Byte | Byte | Byte | Byte | Byte | Byte |
- 9     | Byte | Byte | Byte | Byte | Byte | Byte | Byte | Byte |
-10     | Byte | Byte | Byte | Byte | Byte | Byte | Byte | Byte |
-11     | Byte | Byte | Byte | Byte | Byte | Byte | Byte | Byte |
-
-
-
-
-
